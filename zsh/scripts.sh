b3ee47be049967712a5b67b480134f5c20e1e432
Garrett Nelson <gjnels@proton.me>
2023-05-19 11:05:03 -0400

    Create script to choose a tmuxp layout using fzf

--- zsh/scripts.sh
+++ zsh/scripts.sh
@@ -5,3 +5,38 @@
 compress() {
     tar cvzf $1.tar.gz $1
 }
+
+
+
+# choose tmuxp layout using fzf
+ftmuxp() {
+    # return if already in tmux session
+    if [[ -n $TMUX ]]; then
+        echo "Already in a tmux session. Nice try :)"
+        return
+    fi
+
+    # get tmuxp layout names (removing .yml extension)
+    ID="$(ls $XDG_CONFIG_HOME/tmuxp | sed -e 's/\.yml$//')"
+
+    # start basic tmux session if no tmuxp layouts found
+    if [[ -z "$ID" ]]; then
+        tmux new-session
+    fi
+
+    msg="Create New Session"
+
+    # get desired tmuxp layout
+    ID="${msg}\n$ID"
+    ID="$(echo $ID | fzf | cut -d: -f1)"
+
+    if [[ "$ID" = "${msg}" ]]; then
+        # chose to start new session
+        tmux new-session
+    elif [[ -n "$ID" ]]; then
+        # load chosen tmuxp layout
+        tmuxp load "$ID"
+    fi
+
+    # when nothing chosen (hitting ESC in fzf menu), script exits
+}
