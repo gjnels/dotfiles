ee19213f8a6e2fdcf6e4e43ee48b05f5c8a36db4
Garrett Nelson <gjnels@proton.me>
2023-05-20 17:19:39 -0400

    Prevent unwanted env variables when installing on macos

--- install.sh
+++ install.sh
@@ -4,22 +4,21 @@
 
 
 
-#---------------------------------------
+#-------------------------------------------------------------------------------
 #-- Source Environment Variables
 
-SCRIPT_DIR=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
+SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 source "$SCRIPT_DIR/zsh/zshenv"
 
 
 
-#---------------------------------------
+#-------------------------------------------------------------------------------
 #-- Neovim
 
 # remove Neovim config and data if present
 rm -rf "$XDG_CONFIG_HOME/nvim"
-rm -rf "$XDG_DATA_HOME/nvim"
 
-# create undo directory
+# create undo directory if it doesn't exist
 mkdir -p "$XDG_DATA_HOME/nvim/undo"
 
 # link Neovim config
@@ -27,29 +26,7 @@ ln -s "$DOTFILES/nvim" "$XDG_CONFIG_HOME"
 
 
 
-#---------------------------------------
-#-- X11
-
-# remove directory before creating link
-rm -rf "$XDG_CONFIG_HOME/X11"
-
-# link entire X11 directory
-ln -s "$DOTFILES/X11" "$XDG_CONFIG_HOME"
-
-
-
-#---------------------------------------
-#-- i3
-
-# remove directory before creating link
-rm -rf "$XDG_CONFIG_HOME/i3"
-
-# link entire i3 directory
-ln -s "$DOTFILES/i3" "$XDG_CONFIG_HOME"
-
-
-
-#---------------------------------------
+#-------------------------------------------------------------------------------
 #-- Zsh
 
 # create directory if it doesn't exist
@@ -63,29 +40,7 @@ ln -sf "$DOTFILES/zsh/zshrc" "$XDG_CONFIG_HOME/zsh/.zshrc"
 
 
 
-#---------------------------------------
-#-- Dunst Notifications
-
-mkdir -p "$XDG_CONFIG_HOME/dunst"
-ln -sf "$DOTFILES/dunst/dunstrc" "$XDG_CONFIG_HOME/dunst/dunstrc"
-
-
-
-#---------------------------------------
-#-- Rofi
-
-# link config file
-mkdir -p "$XDG_CONFIG_HOME/rofi"
-ln -sf "$DOTFILES/rofi/config.rasi" "$XDG_CONFIG_HOME/rofi/config.rasi"
-
-# link themes
-mkdir -p "$XDG_DATA_HOME/rofi"
-rm -rf "$XDG_DATA_HOME/rofi/themes"
-ln -s "$DOTFILES/rofi/themes" "$XDG_DATA_HOME/rofi"
-
-
-
-#---------------------------------------
+#-------------------------------------------------------------------------------
 #-- Git
 
 rm -rf "$XDG_CONFIG_HOME/git"
@@ -93,7 +48,7 @@ ln -s "$DOTFILES/git" "$XDG_CONFIG_HOME"
 
 
 
-#---------------------------------------
+#-------------------------------------------------------------------------------
 #-- Tmux
 
 mkdir -p "$XDG_CONFIG_HOME/tmux"
@@ -103,3 +58,44 @@ ln -sf "$DOTFILES/tmux/tmux.conf" "$XDG_CONFIG_HOME/tmux/tmux.conf"
 [ ! -d "$XDG_CONFIG_HOME/tmux/plugins/tpm" ] \
     && git clone https://github.com/tmux-plugins/tpm \
     "$XDG_CONFIG_HOME/tmux/plugins/tmux"
+
+
+
+#-------------------------------------------------------------------------------
+#-- Linux Environment Variables
+
+if [[ $(uname) == "Linux" ]]; then
+    #-- X11
+    # remove directory before creating link
+    rm -rf "$XDG_CONFIG_HOME/X11"
+
+    # link entire X11 directory
+    ln -s "$DOTFILES/X11" "$XDG_CONFIG_HOME"
+
+
+
+    #-- i3
+    # remove directory before creating link
+    rm -rf "$XDG_CONFIG_HOME/i3"
+
+    # link entire i3 directory
+    ln -s "$DOTFILES/i3" "$XDG_CONFIG_HOME"
+
+
+
+    #-- Dunst Notifications
+    mkdir -p "$XDG_CONFIG_HOME/dunst"
+    ln -sf "$DOTFILES/dunst/dunstrc" "$XDG_CONFIG_HOME/dunst/dunstrc"
+
+
+
+    #-- Rofi
+    # link config file
+    mkdir -p "$XDG_CONFIG_HOME/rofi"
+    ln -sf "$DOTFILES/rofi/config.rasi" "$XDG_CONFIG_HOME/rofi/config.rasi"
+
+    # link themes
+    mkdir -p "$XDG_DATA_HOME/rofi"
+    rm -rf "$XDG_DATA_HOME/rofi/themes"
+    ln -s "$DOTFILES/rofi/themes" "$XDG_DATA_HOME/rofi"
+fi
