9c831f0c71067ae2f6870b8c2a5aebeac03d37fb
Garrett Nelson <gjnels@proton.me>
2023-05-30 14:46:04 -0400

    nvim: Switch from mini.pairs to nvim-autopairs

--- nvim/lua/plugins/core.lua
+++ nvim/lua/plugins/core.lua
@@ -3,4 +3,12 @@ return {
     'nvim-lua/plenary.nvim',
     lazy = true,
   },
+
+  -- auto-pairs
+  {
+    'windwp/nvim-autopairs',
+    event = 'InsertEnter',
+    opts = require('plugins.configs.nvim-autopairs').opts,
+    config = require('plugins.configs.nvim-autopairs').config,
+  },
 }
--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -8,13 +8,6 @@ return {
   -- Detect tabstop and shiftwidth automatically
   'tpope/vim-sleuth',
 
-  -- auto pairs
-  {
-    'echasnovski/mini.pairs',
-    event = 'VeryLazy',
-    opts = {},
-  },
-
   -- NOTE: This is where your plugins related to LSP can be installed.
   --  The configuration is done below. Search for lspconfig to find it below.
   {
