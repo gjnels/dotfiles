b461d3418743ed534bf1a8f79680fae3521b9ccf
Garrett Nelson <gjnels@proton.me>
2023-06-07 15:22:50 -0400

    nvim: Switch to LazyVim

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -1,61 +1,44 @@
-local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  -- Clone lazy.nvim
-  local output = vim.fn.system({
-    'git',
-    'clone',
-    '--filter=blob:none',
-    'https://github.com/folke/lazy.nvim.git',
-    '--branch=stable', -- latest stable release
-    lazypath,
-  })
-  -- Check for lazy.nvim cloning error
-  if vim.api.nvim_get_vvar('shell_error') ~= 0 then
-    vim.api.nvim_err_writeln('Error cloning lazy.nvim repo...\n\n' .. output)
-  end
-
-  -- Load Mason and Treesitter after lazy installs plugins
-
-  -- temporarily show command line
-  local oldcmdheight = vim.opt.cmdheight:get()
-  vim.opt.cmdheight = 1
-
-  vim.notify('Plugins are installing...')
-
-  vim.api.nvim_create_autocmd('User', {
-    desc = 'Load Mason and Treesitter after Lazy intalls plugins',
-    once = true, -- only run autocmd one time
-    pattern = 'LazyInstall', -- after Lazy installs plugins
-    callback = function()
-      vim.cmd.bw() -- wipe all buffers
-      vim.opt.cmdheight = oldcmdheight -- reset cmdheight
-      -- Load Mason and Treesitter
-      vim.tbl_map(function(module)
-        pcall(require, module)
-      end, { 'nvim-treesitter', 'mason' })
-      -- Notify Mason installing packages
-      vim.schedule(function()
-        vim.notify('Mason is installing preconfigured packages, check status with :Mason')
-      end)
-    end,
-  })
+  -- bootstrap lazy.nvim
+  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
 end
-vim.opt.rtp:prepend(lazypath)
+vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 
-local spec = {
-  { import = 'plugins' },
-}
+require("lazy").setup({
+  spec = {
+    -- add LazyVim and import its plugins
+    { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+    
+    -- import any LazyVim extras modules here
+    -- { import = "lazyvim.plugins.extras.lang.typescript" },
+    -- { import = "lazyvim.plugins.extras.lang.json" },
 
-local colorscheme = { 'tokyonight' }
-
-require('lazy').setup({
-  spec = spec,
-  defaults = { lazy = false },
-  install = { colorscheme = colorscheme },
-  ui = {
-    border = 'rounded',
+    -- import/override with your plugins
+    { import = "plugins" },
   },
+  defaults = {
+	  -- All LazyVim plugins are lazy loaded
+	  -- don't lazy load custom plugins by default
+    lazy = false,
+    
+    version = false, -- always use the latest git commit
+  },
+  install = { colorscheme = { "tokyonight", "habamax" } },
+  checker = { enabled = true }, -- automatically check for plugin updates
   performance = {
-    disabled_plugins = { 'tohtml', 'gzip', 'zipPlugin', 'netrwPlugin', 'tarPlugin', 'tutor' },
+    rtp = {
+      -- disable some rtp plugins
+      disabled_plugins = {
+        "gzip",
+        "matchit",
+        "matchparen",
+        "netrwPlugin",
+        "tarPlugin",
+        "tohtml",
+        "tutor",
+        "zipPlugin",
+      },
+    },
   },
 })
